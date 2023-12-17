114f71c8debe292c5ba1ecd8aea81487a0bf8a48
SAwandya <sachilaawandya@gmail.com>
2023-12-17 13:12:12 +0530

    Fetch the genres

---
+++ src/hooks/useGenres.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+const useGenres = () => {
+  const [genres, setGenres] = useState([]);
+  const [error, setErrors] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    apiClient
+      .get("/genres", { signal: controller.signal })
+      .then((res) => {
+        setGenres(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setErrors(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenres;
